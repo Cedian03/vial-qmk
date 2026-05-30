af6483a1ec57d5da11213e0d471774c5da9a5c38
Cedian03 <nyaalgotjohansson@gmail.com>
2026-05-30 20:22:45 +0200

    Add custom DZ60 keymap

---
+++ keyboards/dz60/keymaps/cedian03/config.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE2, 0xAA, 0x19, 0xF5, 0x60, 0xFD, 0x8B, 0x67}
+
+// Esc + Enter
+#define VIAL_UNLOCK_COMBO_ROWS {0, 2}
+#define VIAL_UNLOCK_COMBO_COLS {0, 13}
+
+#define TAPPING_TERM 300
+#define QUICK_TAP_TERM 0
+
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
